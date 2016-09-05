0b62d261571995a4f54cfadbc2d9589123752438
lewis007 <lewis.lu.fs@icloud.com>
2016-09-05 15:26:44 +0800

    formatter source

--- gs-uploading-files/src/main/java/hello/Application.java
+++ gs-uploading-files/src/main/java/hello/Application.java
@@ -18,10 +18,11 @@ public class Application {
 	}
 
 	@Bean
-	CommandLineRunner init(StorageService storageService) {
+	public CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
 			storageService.deleteAll();
 			storageService.init();
 		};
 	}
+
 }
