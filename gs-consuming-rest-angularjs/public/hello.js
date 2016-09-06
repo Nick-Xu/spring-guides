60f987eb81ec08e21898d2ef1d2985791433230c
lewis007 <lewis.lu.fs@icloud.com>
2016-09-06 09:47:22 +0800

    Sync to newly AngularJS link from 'cdn.bootcss.com'

--- gs-consuming-rest-angularjs/public/hello.js
+++ gs-consuming-rest-angularjs/public/hello.js
@@ -1,7 +1,5 @@
-function Hello($scope, $http) {
-
-	$http.get('http://localhost:8080/greeting').success(function(data) {
-		$scope.greeting = data;
+angular.module('demo', []).controller('Hello', function($scope, $http) {
+	$http.get('http://localhost:8080/greeting').then(function(response) {
+		$scope.greeting = response.data;
 	});
-
-}
+});
