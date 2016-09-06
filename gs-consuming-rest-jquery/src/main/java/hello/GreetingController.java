652f65caf67276ef44e3417a7771cccd7fdd2c0e
lewis007 <lewis.lu.fs@icloud.com>
2016-09-06 11:42:56 +0800

    change webservice's request from RequestMapping to GetMapping

--- gs-consuming-rest-jquery/src/main/java/hello/GreetingController.java
+++ gs-consuming-rest-jquery/src/main/java/hello/GreetingController.java
@@ -3,7 +3,7 @@ package hello;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,7 +13,7 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
+	@GetMapping("/greeting")
 	public @ResponseBody Greeting greeting(@RequestParam(required = false, defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
